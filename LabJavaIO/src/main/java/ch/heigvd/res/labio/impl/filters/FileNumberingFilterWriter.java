46170d94aab8367f18a7db460f5d0a46a40a10f0
Gaetan Bacso <gaeba95@gmail.com>
2019-03-11 16:32:57 +0100

    [TOREVIEW] Implements FileNumberingFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
 
+
 /**
  * This class transforms the streams of character sent to the decorated writer.
  * When filter encounters a line separator, it sends it to the decorated writer.
@@ -18,24 +19,58 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private boolean start;
+  private boolean back;
+  private int cnt;
+  
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    cnt = 1;
+    start = true;
+    back = false;
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	  char[] cbuf = str.toCharArray();
+      this.write(cbuf,off,len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	  for(int i = off; i < off + len; i++){
+          this.write(cbuf[i]);
+      }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	  if(start){
+          start = false;
+          this.out.write(cnt++ + "\t");
+      }
+	  
+	  if(c == '\r') {
+		  if(back){
+              this.out.write("\r" + cnt++ + "\t");
+          }
+		  back = true;
+	  } else if(c == '\n') {
+		  if(back) {
+			  this.out.write("\r\n" + cnt++ + "\t");
+			  back = false;
+		  } else {
+			  this.out.write("\n" + cnt++ + "\t");
+		  }
 
+	  } else {
+		  if(back) {
+              this.out.write("\r"  + cnt++ + "\t" + (char)c);
+              back = false;
+		  } else {
+			  this.out.write((char) c);
+		  }
+	  }
+  }
 }
